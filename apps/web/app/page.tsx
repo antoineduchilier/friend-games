bb494538feaeed6bff99717ffdf9ea5f3a083ec2
antoine <antoineduchilier@gmail.com>
2026-07-28 16:45:10 +0200

    Homepage creation

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,78 @@
-const Toto = () => <a href="/darts">Go to darts</a>;
+import { Card } from 'antd';
+import Link from 'next/link';
+import styles from './pages/homepage/Homepage.module.css';
 
-export default Toto;
+const Homepage = () => {
+  return (
+    <div className={styles['homepage']}>
+      <h3 className={styles['homepage-title']}>Bienvenue sur Friend Games !</h3>
+      <div className={styles['homepage-cards']}>
+        {/* <div className={styles['homepage-cards-grid']}> */}
+        <Card
+          cover={
+            <img
+              className={styles['homepage-image-card']}
+              src="/images/darts.jpg"
+              alt="Jeu de fléchettes"
+              draggable={false}
+            />
+          }
+          className={styles['homepage-card']}
+        >
+          <Link href="/pages/darts">
+            <b>Go to Darts</b>{' '}
+          </Link>
+        </Card>
+        <Card
+          cover={
+            <img
+              className={styles['homepage-image-card']}
+              src="/images/comingsoon.jpg"
+              alt="Jeu de fléchettes"
+              draggable={false}
+            />
+          }
+          className={styles['homepage-card']}
+        >
+          <Link href="/">
+            <b>Coming Soon</b>
+          </Link>
+        </Card>
+        {/* </div>
+        <div className={styles['homepage-cards-grid']}> */}
+        <Card
+          cover={
+            <img
+              src="/images/comingsoon.jpg"
+              alt="Jeu de fléchettes"
+              draggable={false}
+              className={styles['homepage-image-card']}
+            />
+          }
+          className={styles['homepage-card']}
+        >
+          <Link href="/">
+            <b>Coming Soon</b>
+          </Link>
+        </Card>
+        <Card
+          cover={
+            <img
+              src="/images/comingsoon.jpg"
+              alt="Jeu de fléchettes"
+              draggable={false}
+              className={styles['homepage-image-card']}
+            />
+          }
+          className={styles['homepage-card']}
+        >
+          <Link href="/">
+            <b>Coming Soon</b>
+          </Link>
+        </Card>
+        {/* </div> */}
+      </div>
+    </div>
+  );
+};
+export default Homepage;
