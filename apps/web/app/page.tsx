1161dac8c105730f8a2203badc7f91e8297b6b24
antoine <antoineduchilier@gmail.com>
2026-08-25 15:43:41 +0200

    Added homepage modifications from MR

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,6 +5,33 @@ import styles from './pages/homepage/Homepage.module.css';
 import GameCard from './pages/homepage/component/gamecard/GameCard';
 
 const Homepage = () => {
+  const games = [
+    {
+      href: '/pages/darts',
+      gameName: 'Fléchettes',
+      gameDescription: '301, 501, Cricket. Visez le centre !',
+      gamePlayer: '2-8 joueurs',
+      selectIcon: <TargetIcon />,
+      color: 'var(--color-primary)',
+      opacityColor: 'var(--color-primary10)',
+      backgroundColor: '#FFFFFF',
+      titleColor: '#111827',
+      subtitleColor: '#4B5563',
+    },
+    {
+      href: '/',
+      gameName: 'Belote',
+      gameDescription: 'Annonces, capots et belote-rebelote',
+      gamePlayer: '2 ou 4 joueurs (équipes)',
+      selectIcon: <CrossIcon />,
+      color: '#9CA3AF',
+      opacityColor: '#E5E7EB',
+      backgroundColor: '',
+      titleColor: '#6B7280',
+      subtitleColor: '#9CA3AF',
+    },
+  ];
+
   return (
     <div className={styles['homepage']}>
       <div className={styles['homepage-logo-title']}>
@@ -16,6 +43,7 @@ const Homepage = () => {
           <p className={styles['homepage-content-subtitle']}>Compteur de points modernes</p>
         </div>
       </div>
+
       <div>
         <div className={styles['homepage-content-newgame']}>
           <h1 className={styles['homepage-newgame-title']}>
@@ -25,24 +53,11 @@ const Homepage = () => {
             Sélectionnez votre jeu pour commencer la partie
           </p>
         </div>
-        <GameCard
-          href={'/pages/darts'}
-          gameName={'Fléchettes'}
-          gameDescription={'301, 501, Cricket. Visez le centre !'}
-          gamePlayer={'2-8 joueurs'}
-          selectIcon={<TargetIcon />}
-          color={'var(--color-primary)'}
-          opacityColor={'var(--color-primary10)'}
-        />
-        <GameCard
-          href={'/'}
-          gameName={'Belote'}
-          gameDescription={'Annonces, capots et belote-rebelote'}
-          gamePlayer={'2 ou 4 joueurs (équipes)'}
-          selectIcon={<CrossIcon />}
-          color={'var(--color-secondary'}
-          opacityColor={'var(--color-secondary10)'}
-        />
+      </div>
+      <div className={styles['homepage-gamecard']}>
+        {games.map((game) => (
+          <GameCard key={game.gameName} {...game} />
+        ))}
       </div>
     </div>
   );
--- apps/web/app/pages/homepage/component/gamecard/GameCard.tsx
+++ apps/web/app/pages/homepage/component/gamecard/GameCard.tsx
@@ -1,10 +1,9 @@
-import { Card } from 'antd';
 import { Url } from 'next/dist/shared/lib/router/router';
 import Link from 'next/link';
 import { JSX } from 'react';
 import RightArrow from '../../../../../public/images/RightArrow';
 import styles from './GameCard.module.css';
-interface GameCardProps {
+type GameCardProps = {
   href: Url;
   gameName: string;
   gameDescription: string;
@@ -12,7 +11,10 @@ interface GameCardProps {
   selectIcon: JSX.Element;
   color: string;
   opacityColor: string;
-}
+  backgroundColor: string;
+  titleColor: string;
+  subtitleColor: string;
+};
 
 const GameCard = ({
   href,
@@ -22,41 +24,39 @@ const GameCard = ({
   selectIcon,
   color,
   opacityColor,
+  backgroundColor,
+  titleColor,
+  subtitleColor,
 }: GameCardProps) => {
   return (
     <div
-      className={styles['homepage-card']}
       style={
-        { '--define-color': color, '--define-opacity-color': opacityColor } as React.CSSProperties
+        {
+          '--define-color': color,
+          '--define-opacity-color': opacityColor,
+          '--define-background-color': backgroundColor,
+          '--define-title-color': titleColor,
+          '--define-subtitle-color': subtitleColor,
+        } as React.CSSProperties
       }
     >
       <Link href={href}>
-        <Card style={{ borderRadius: '24px' }}>
-          <div className={styles['homepage-card-content']}>
-            <div className={styles['homepage-card-left']}>
-              <div className={styles['homepage-background-card-icon']}>
-                <div className={styles['homepage-card-icon']}>{selectIcon}</div>
-              </div>
-              <div>
-                <h2 className={styles['homepage-game-title']}>
-                  {gameName} <RightArrow className={styles['homepage-arrow']} />
-                </h2>
-                <p className={styles['homepage-game-subtitle']}>{gameDescription} </p>
-                <div className={styles['homepage-card-players']}>
-                  <div
-                    style={{
-                      width: '12px',
-                      height: '12px',
-                      borderRadius: '12px',
-                      backgroundColor: 'var(--define-color)',
-                    }}
-                  />
-                  <p>{gamePlayer}</p>
-                </div>
+        <div className={styles['homepage-card-content']}>
+          <RightArrow className={styles['homepage-arrow']} />
+          <div className={styles['homepage-card-left']}>
+            <div className={styles['homepage-background-card-icon']}>
+              <div className={styles['homepage-card-icon']}>{selectIcon}</div>
+            </div>
+            <div>
+              <h2 className={styles['homepage-game-title']}>{gameName}</h2>
+              <p className={styles['homepage-game-subtitle']}>{gameDescription} </p>
+              <div className={styles['homepage-card-players']}>
+                <div className={styles['homepage-card-dot-before-player']} />
+                <p>{gamePlayer}</p>
               </div>
             </div>
           </div>
-        </Card>
+        </div>
       </Link>
     </div>
   );
